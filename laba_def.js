6e455c26ac05265a0329358c5aecaa5f8a9ec50f
Dima1Garbar <50574247+Dima1Garbar@users.noreply.github.com>
2020-02-06 12:29:46 +0200

    Update laba_def.js

--- laba_def.js
+++ laba_def.js
@@ -1,18 +1,19 @@
 var func = function (a, b, c){
     if (a === true){
-        console.log(b+c)
+        console.log(b+c);
     }
     else if (a && b === 5){
-        console.log(5)
+        console.log(5);
     }
 }
 function func_2(a, b, c){
     if (a != b){
-        console.log(a*b*c)
+        console.log(a*b*c);
     }
-    else if (a || b || c === 5){
-        console.log("Внатурі 5 :)")
+    else if (a === 5 || c === 5){
+        console.log("Внатурі 5 :)");
     }
+    else console.log(arguments[1])
 }
 func(true, "i am ", 5);
-func_2(5, 5, 5);
+func_2(3, 3, 2);
